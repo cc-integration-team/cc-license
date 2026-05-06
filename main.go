f4eb05e0cf6be85d57b416bc71b39310a9a4b6e6
dangquyitt <dangquyitt@gmail.com>
2026-05-06 12:02:19 +0700

    Implement runner structure and signal handling for graceful shutdown

--- main.go
+++ main.go
@@ -1,7 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"log/slog"
+	"os"
+	"os/signal"
+)
+
+func NewRunner() *Runner {
+	return &Runner{}
+}
+
+type Runner struct {
+	queue chan int
+}
+
+func (r *Runner) Run() {
+	for data := range r.queue {
+		slog.Info("Processing data", "data", data)
+	}
+}
+
+func (r *Runner) Stop() {
+	close(r.queue)
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+	runner := NewRunner()
+	go runner.Run()
+	for i := 0; i < 10; i++ {
+		runner.queue <- i
+	}
+	runner.Stop()
+
+	<-ctx.Done()
 }
